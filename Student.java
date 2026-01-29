bc84cd47017c871fba1cacc91440840126c4091f
maha <alhawamdehmaha8@gmail.com>
2026-01-29 14:01:51 +0300

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,34 @@
+public class Student {
+
+    private int id;
+    private String name;
+    private String major;
+    private double gpa;
+
+    public Student(int id, String name, String major, double gpa) {
+        this.id = id;
+        this.name = name;
+        this.major = major;
+        this.gpa = gpa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+}
